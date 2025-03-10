bf108fbd6bb3ff409d78c7d35e30ac7708d7fca2
Arno-ATU <190516820+Arno-ATU@users.noreply.github.com>
2025-03-10 15:01:34 +0000

    WIP adding index.cshtml

--- Pages/Members/Index.cshtml.cs
+++ Pages/Members/Index.cshtml.cs
@@ -1,29 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GolfBookingApp.Data;
 using GolfBookingApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace GolfBookingApp.Pages.Members
 {
-    public class IndexModel : PageModel
+    public class IndexModel:PageModel
     {
-        private readonly GolfBookingApp.Data.GolfClubContext _context;
+        private readonly GolfClubContext _context;
 
-        public IndexModel(GolfBookingApp.Data.GolfClubContext context)
+        public IndexModel(GolfClubContext context)
         {
             _context = context;
         }
 
-        public IList<Member> Member { get;set; } = default!;
+        public IList<Member> Members { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Member = await _context.Members.ToListAsync();
+            if (_context.Members != null)
+            {
+                Members = await _context.Members.ToListAsync();
+            }
         }
     }
 }
